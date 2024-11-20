031f4d4b9c896995bed67bb52909360a6e645def
edersonbadeca <edersonbadeca@gmail.com>
2024-11-20 19:00:19 -0300

    docs: add env example

--- src/backend/config/app_config.py
+++ src/backend/config/app_config.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@0.0.0.0:5432/mydatabase'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
